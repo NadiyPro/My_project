bd24ec875a3e9b3cc43e0c075c96092a24b86eb0
NadiyPro <siroviyn13@gmail.com>
2025-06-30 15:33:21 +0300

    create

--- project1/project1.js
+++ project1/project1.js
@@ -1,4 +1,17 @@
-window.addEventListener('scroll',
-    ()=> {
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
-})
+window.addEventListener('DOMContentLoaded', () => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    gsap.to('.box', {
+        scrollTrigger: {
+            trigger: '.box',
+            start: 'top top',
+            end: 'bottom top',
+            scrub: true,
+        },
+    });
+
+    // Оновлення змінної --scrollTop під час скролу
+    window.addEventListener('scroll', () => {
+        document.documentElement.style.setProperty('--scrollTop', `${window.scrollY}px`);
+    });
+});
